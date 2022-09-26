0d015c4558b65e85de19473778acba0780b22a23
nabilah3455 <nabilah3455@gmail.com>
2022-09-26 09:58:29 +0700

    show email contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,31 +15,48 @@ import ContactForm from "./ContactForm";
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
+
+const addKeterangan = (phone, email) => {
+  return phone + `\n` + email;
+};
+
 const Contact = ({ contacts, addContact }) => {
   // Contact berisi foto, nama, telepon, dan email
   return (
     <>
-      <Grid container spacing={2} sx={{marginTop: '2rem'}}>
+      <Grid container spacing={2} sx={{ marginTop: "2rem" }}>
         <Grid item xs={5}>
           <ContactForm addContact={addContact} />
         </Grid>
         <Grid item xs={7}>
           {/* <Container maxWidth="sm" sx={{ background: "#dbf6f0" }}> */}
-            <List sx={{margin: '0 5rem', background: "#dbf6f0"}}>
-              {contacts.map((el) => {
-                return (
-                  <ListItem sx={{ borderBottom: '1px solid #dbd9d4'}}>
-                    <ListItemAvatar>
-                      <Avatar sx={{width: '65px', height: '65px', marginRight: '3rem'}}>
-                        <img src={el.photo} width={65} />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={el.name} secondary={el.phone} />
-                    {/* <span>{el.email}</span> */}
-                  </ListItem>
-                );
-              })}
-            </List>
+          <List sx={{ margin: "0 5rem", background: "#dbf6f0" }}>
+            {contacts.map((el) => {
+              return (
+                <ListItem sx={{ borderBottom: "1px solid #dbd9d4" }}>
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{
+                        width: "65px",
+                        height: "65px",
+                        marginRight: "3rem",
+                      }}
+                    >
+                      <img src={el.photo} width={65} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={el.name}
+                    secondary={<div>
+                      <div>{el.phone}</div>
+                      <div>{el.email}</div>
+                    </div>}
+                  ></ListItemText>
+                  {/* <span>{el.email}</span> */}
+                </ListItem>
+              );
+            })}
+          </List>
           {/* </Container> */}
         </Grid>
       </Grid>
